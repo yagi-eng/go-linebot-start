a5f8bbb77df8ebf6cae504864b9aae4065b2108d
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-10-04 14:29:05 +0900

    オウム返しを実装

--- answer/main.go
+++ answer/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
 	// ハンドラの登録
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
 
 	fmt.Println("http://localhost:8080 で起動中...")
 	// HTTPサーバを起動
@@ -18,4 +21,41 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	msg := "Hello World!!!!"
 	fmt.Fprintf(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	// BOTを初期化
+	bot, err := linebot.New(
+		"(自分のシークレットを入力)",
+		"(自分のアクセストークンを入力)",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// リクエストからBOTのイベントを取得
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		// イベントがメッセージの受信だった場合
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+			// 他にもスタンプや画像、位置情報など色々受信可能
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yagi-eng/go-linebot-start
 
 go 1.15
+
+require github.com/line/line-bot-sdk-go v7.6.0+incompatible
